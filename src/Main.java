01acc612fe1aa80de73339fa6e2d0c413346b779
Melike İsbir <melikeisbr@gmail.com>
2023-12-01 20:48:08 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 //import fibonaccidongu.fibonacci;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String [  ]  args) {
@@ -8,7 +9,7 @@ public class Main {
         //     a b c
         //         a b c
 
-        int a = 1;
+      /* int a = 1;
         int b = 1;
         int c;
         System.out.println(a);
@@ -19,6 +20,64 @@ public class Main {
             a = b;
             b = c;
             System.out.println(c);
+        } */
+
+       //FOR DONGUSU
+      /*  int i = 0;
+
+        for (int j = 0; j < 5;   m++) {
+            System.out.println("i: " + i + ", m: " + m);
+            i++;
+        }
+        for (int k = 0; k < 5; m++) {
+                ++i;
+                System.out.println("i: " + i + ", m: " + m);
+        }*/
+
+       /* System.out.println("Min value: "  +  Byte.MIN_VALUE + "Max value: "  + Byte.MAX_VALUE);      //byte
+        System.out.println("Min value: "  +  Short.MIN_VALUE + "Max value: "  + Short.MAX_VALUE);  //short
+        System.out.println("Min value: "  +   Integer.MIN_VALUE + "Max value: "  + Integer.MAX_VALUE);   //integer
+        System.out.println("Min value: "  +   Long.MIN_VALUE + "Max value: "  +  Long.MAX_VALUE);  //long
+        System.out.println("Min value: "  +   Float.MIN_VALUE + "Max value: "  +   Float.MAX_VALUE);   //float
+        System.out.println("Min value: "  +   Double.MIN_VALUE + "Max value: "  +   Double.MAX_VALUE);   //double
+        System.out.println("Min value: "  +   (int)  Character.MIN_VALUE + "Max value: "  +   (int)  Character.MAX_VALUE);    //character   Unicode                 tablosundaki karşılığı
+        System.out.println("\nboolean:");     // boolean veri tipi  sadece true ve  false alır, min ve max değeri yoktur.  */
+
+
+
+
+        // 3 sayının ortalaması
+        Scanner scan = new Scanner(System.in);
+       int sayi, toplam = 0;
+
+        System.out.print("Girmek istediğiniz sayı adedini giriniz: ");
+        int adet = scan.nextInt();
+
+        for(int i = 0; i < adet; i++)
+        {
+            System.out.print((i + 1) + ". sayiyi giriniz: ");
+            sayi = scan.nextInt();
+            toplam += sayi;
+        }
+        System.out.println("Sayıların ortalaması = " + ((float)toplam / adet));
+
+
+
+        // Vize ve Final notlarının ortalamasıyla 50 üstünü geçti mi?
+        System.out.print("Vize notunuzu giriniz: ");
+        int vize = scan.nextInt();
+        System.out.print("Final notunu giriniz: ");
+        int fnal = scan.nextInt();
+
+        double not = (vize * 0.4) + (fnal * 0.6);
+
+        System.out.println("Yıl sonu notunuz = " + not);
+
+        if(not > 50) {
+            System.out.println("Dersi geçtiniz.");
+        }
+        else if(not < 50) {
+            System.out.println("Dersten kaldınız.");
         }
     }
 }
