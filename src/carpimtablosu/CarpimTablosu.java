c096c186d397aada2915df8b6abcba851fbd6832
Melike İsbir <melikeisbr@gmail.com>
2023-12-06 19:42:38 +0300

    update

--- src/carpimtablosu/CarpimTablosu.java
+++ src/carpimtablosu/CarpimTablosu.java
@@ -3,10 +3,16 @@ package carpimtablosu;
 public class CarpimTablosu {
     public void CarpimTablosuOlustur(){
 
-        int carpim = 1;
-        for (int i = 1; i < 10; i++) {
-            carpim += i*carpim;
+        int x = 1;
+        int y=1;
+        for (int i = 1; i< 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                int sayi1 = x + j;
+                int sayi2 = y + i;
+                int carpim = sayi1 * sayi2;
+                System.out.println( sayi1 + "*" + sayi2 + "=" +  carpim);
+            }
+            System.out.println();
         }
-        System.out.println("Sayıların Çarpımı=" + carpim);
     }
 }
